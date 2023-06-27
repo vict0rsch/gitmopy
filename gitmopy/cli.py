9adaa2108b2dc0848beb85325349c2ab9f7d7845
vict0rsch <vsch@pm.me>
2023-06-27 09:44:29 -0400

    🐛 (cli): Check upstream before console status prints

--- gitmopy/cli.py
+++ gitmopy/cli.py
@@ -174,13 +174,13 @@ def push_cli(repo, remote):
             if remote.name in selected_remotes:
                 wait = col(f"Pushing to remote {remote.name}...", "o")
                 done = col(f"Pushed to remote {remote.name}", "b", True)
+                set_upstream = False
+                if not remote_upstreams[remote.name]:
+                    set_upstream = set_upstream_prompt(remote.name)
                 with console.status(wait):
                     # push to remote, catch exception if it fails to be able to
                     # 1. continue pushing to other remotes
                     # 2. potentially set the upstream branch
-                    set_upstream = False
-                    if not remote_upstreams[remote.name]:
-                        set_upstream = set_upstream_prompt(remote.name)
                     cre = None
                     if set_upstream:
                         with CatchRemoteException(remote.name) as cre:
