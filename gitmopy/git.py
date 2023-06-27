7d4bf2931c4c7c3abac74d10da83b34c59c5bc52
vict0rsch <vsch@pm.me>
2023-06-27 10:04:51 -0400

    🐛 (git): Fix upstream check using `refs`

--- gitmopy/git.py
+++ gitmopy/git.py
@@ -87,20 +87,15 @@ def has_upstreams(
 
     fetch_all(repo)
     remote_has_upstream = {
-        r.name if isinstance(r, Remote) else r: None for r in remotes
+        r.name if isinstance(r, Remote) else r: False for r in remotes
     }
-    for r in remotes:
-        if isinstance(r, Remote):
-            r = r.name
-        assert isinstance(r, str)
-        try:
-            repo.git.branch("--list", f"{r}/{branch_name}")
-            remote_has_upstream[r] = True
-        except GitCommandError as e:
-            if "fatal: not a valid revision" in str(e):
-                remote_has_upstream[r] = False
-            else:
-                raise e
+    remote_refs = {
+        ref.remote_name: ref.name.removeprefix(ref.remote_name + "/") == branch_name
+        for ref in repo.refs
+        if ref.is_remote()
+    }
+    remote_has_upstream.update(remote_refs)
+
     return remote_has_upstream
 
 
@@ -256,4 +251,4 @@ def format_remotes_diff(repo: Repo) -> str:
                 f"  ↳ local is ahead of {r.name} by {ahead[r.name]} commit(s)\n", "p"
             )
 
-    return s
+    return behind, s
